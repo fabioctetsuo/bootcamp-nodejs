ab3dfbffc56d467fb2ae78cc8554a5426038efe2
Fabio Tetsuo <fabioctetsuo@gmail.com>
2019-05-16 00:30:52 -0300

    feat: form validation

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const validate = require('express-validation');
 const controllers = require('./app/controllers');
+const validators = require('./app/validators');
 
 // Middleware Imports
 const { validateToken } = require('./app/middlewares/auth');
@@ -9,21 +11,25 @@ const {
   UserController, SessionController, AdController, PurchaseController,
 } = controllers;
 
+const {
+  Ad, Purchase, Session, User,
+} = validators;
+
 // Router instance
 const routes = express.Router();
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/users', validate(User), UserController.store);
+routes.post('/sessions', validate(Session), SessionController.store);
 
 // All routes above the following line will use valida token middleware
 routes.use(validateToken);
 
 routes.get('/ads', AdController.index);
 routes.get('/ads/:id', AdController.show);
-routes.post('/ads', AdController.store);
-routes.put('/ads/:id', AdController.update);
+routes.post('/ads', validate(Ad), AdController.store);
+routes.put('/ads/:id', validate(Ad), AdController.update);
 routes.delete('/ads/:id', AdController.destroy);
 
-routes.post('/purchases', PurchaseController.store);
+routes.post('/purchases', validate(Purchase), PurchaseController.store);
 
 module.exports = routes;
